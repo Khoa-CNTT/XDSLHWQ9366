2047e7d4940206f7d9e3c27d6b4513c3aa4972a1
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-03-22 23:13:18 +0700

    updated

---
+++ datn database patches/initial_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO CHUCVUS (MACHUCVU, TENCHUCVU, TRANGTHAI)
+VALUES ('CV001', 'QuanTriVien', true),
+	   ('CV002', 'NhanVienTuyenSinh', true),
+	   ('CV003', 'CongTacVien', true),
+	   ('CV004', 'NhanVienKeToan', true);
+       
+INSERT INTO LINHVUCS (MALINHVUC, TENLINHVUC)
+VALUES ('LV001', 'Lập trình Web (Frontend, Backend, Fullstack)'),
+	   ('LV002', 'Lập trình Mobile (Android, iOS, Cross-platform)'),
+       ('LV003', 'SQL (MySQL, PostgreSQL, SQL Server)'),
+       ('LV004', 'DevOps & CI/CD'),
+       ('LV005', 'Mạng máy tính & An ninh mạng');
