74359b47fbc97f5ddb6c01de265762cf53c49683
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-04-08 00:06:33 +0700

    Add components Navbar, HompageUser

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,5 +1,11 @@
+import HomePageUsers from "./HomePage/HomePageUser";
+
 function App() {
-  return <main></main>;
+  return (
+    <main className="overflow-x-hidden bg-white text-dark">
+      <HomePageUsers />
+    </main>
+  );
 }
 
 export default App;
