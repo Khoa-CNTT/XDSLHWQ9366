b823b9736e0cb8ef21947e7776592d0e69f92fc4
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-04-08 14:50:29 +0700

    Add components Subscribe, edit HomePageUser

--- FrontEnd/src/HomePage/HomePageUser.tsx
+++ FrontEnd/src/HomePage/HomePageUser.tsx
@@ -1,6 +1,7 @@
 import Banner from "../components/Banner/Banner";
 import Hero from "../components/Hero/Hero";
 import Services from "../components/Services/Services";
+import Subscribe from "../components/Subscribe/Subscribe";
 
 const HomePageUsers = () => {
   return (
@@ -8,6 +9,7 @@ const HomePageUsers = () => {
       <Hero />
       <Services />
       <Banner />
+      <Subscribe />
     </div>
   );
 };
