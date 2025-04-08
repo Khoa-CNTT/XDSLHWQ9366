b4fe15bbe1a4c2ec174280834a9951ff94b78e5d
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-04-08 14:47:25 +0700

    Add components Services, edit HomePageUser

--- FrontEnd/src/HomePage/HomePageUser.tsx
+++ FrontEnd/src/HomePage/HomePageUser.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero/Hero";
+import Services from "../components/Services/Services";
 
 const HomePageUsers = () => {
   return (
     <div>
       <Hero />
+      <Services />
     </div>
   );
 };
