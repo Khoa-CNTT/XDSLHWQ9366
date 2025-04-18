fbdd8c94372a21ce8aa1c9cdbb88a06e36686361
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-04-18 15:35:24 +0700

    Add constants de luu data

---
+++ FrontEnd/src/constants/navbarData.tsx
@@ -0,0 +1,29 @@
+const navItems = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Services",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "About us",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Our Team",
+    link: "#",
+  },
+  {
+    id: 5,
+    title: "Contact us",
+    link: "#",
+  },
+];
+
+export default navItems;
