25ed98de8c1ff2efab2ce7ee2c7432f21feb78b4
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-04-23 17:16:19 +0700

    Tach components ExmCard

---
+++ FrontEnd/src/components/ExamCard/ExamCard.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import examItems from "../../constants/examData";
+
+const ExamCard = ({ item }: { item: (typeof examItems)[0] }) => (
+  <div className="bg-white border border-gray-200 rounded-2xl shadow-md hover:shadow-lg transition duration-300 p-5 flex flex-col justify-between">
+    <div>
+      <div className="h-36 bg-blue-50 rounded-lg mb-4 flex items-center justify-center">
+        <span className="text-xl font-semibold text-blue-700 text-center px-2">
+          {item.tenchungchi}
+        </span>
+      </div>
+      <div className="space-y-1">
+        <p>
+          <strong>Exam day :</strong>{" "}
+          {new Date(item.ngaythi).toLocaleDateString()}
+        </p>
+        <p>
+          <strong>Fees :</strong> {item.lephithi.toLocaleString()} VNĐ
+        </p>
+        {/* <p className="text-sm text-gray-600 mt-2">{item.thongtinchitiet}</p> */}
+      </div>
+    </div>
+
+    {/* Buttons */}
+    <div className="mt-4 flex gap-3 justify-between">
+      <Link
+        to={`/exam/${item.malichthi}`}
+        className="flex-1 text-center bg-primary text-white py-2 rounded-lg text-sm font-medium hover:bg-secondary transition"
+      >
+        Detail
+      </Link>
+      <Link
+        to={`/register/${item.malichthi}`}
+        className="flex-1 text-center bg-secondary text-white py-2 rounded-lg text-sm font-medium hover:bg-primary transition"
+      >
+        Register
+      </Link>
+    </div>
+  </div>
+);
+
+export default ExamCard;
