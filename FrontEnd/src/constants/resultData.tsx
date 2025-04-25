20d94736291034f8ca374479ade5e1b3b0889ea7
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-04-25 10:27:56 +0700

    Add resultData

---
+++ FrontEnd/src/constants/resultData.tsx
@@ -0,0 +1,24 @@
+const resultItem = [
+  {
+    id: 1,
+    tenkhoahoc: "Fullstack Web Development",
+    tenlinhvuc: "Web programming",
+    diem: 7,
+    ketqua: "Good",
+  },
+  {
+    id: 2,
+    tenkhoahoc: "SQL from Basic to Advanced",
+    tenlinhvuc: "SQL",
+    diem: 8,
+    ketqua: "Good",
+  },
+  {
+    id: 3,
+    tenkhoahoc: "Mobile Development with Flutter",
+    tenlinhvuc: "Mobile programming",
+    diem: 9,
+    ketqua: "Excellent",
+  },
+];
+export default resultItem;
