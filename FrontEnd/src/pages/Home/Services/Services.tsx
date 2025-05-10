5de6c69c44140fffd3918bb87ccdbe47b926c22f
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-05-10 09:36:46 +0700

    Update Services

--- FrontEnd/src/pages/Home/Services/Services.tsx
+++ FrontEnd/src/pages/Home/Services/Services.tsx
@@ -27,13 +27,14 @@ const Services = () => {
           Services we provide
         </h1>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-8 cursor-pointer">
-          {serviceItems.map((service) => (
+          {serviceItems.map((service, index) => (
             <motion.div
+              key={service.title || index} // Use a unique identifier, e.g., service.id if available
               variants={SlideLeft(service.delay)}
               initial="initial"
               whileInView={"animate"}
               viewport={{ once: true }}
-              className="bg-[#f4f4f4] rounded-2xl flex flex-col gap-4 items-center justify-center p-4 py-7 hover:bg-white hover:scale-110 duration-300 hover:shadow-2xl "
+              className="bg-[#f4f4f4] rounded-2xl flex flex-col gap-4 items-center justify-center p-4 py-7 hover:bg-white hover:scale-110 duration-300 hover:shadow-2xl"
             >
               <div className="text-4xl mb-4">{service.icon}</div>
               <h1 className="text-lg text-center font-semibold px-3">
